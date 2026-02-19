1668ab72bccc85a26afd3be1f6aaa32c82232cf1
Danik499 <danikshcherban499@gmail.com>
2026-02-19 20:46:13 +0200

    changes

--- src/components/RemoteButton.tsx
+++ src/components/RemoteButton.tsx
@@ -3,6 +3,6 @@ import { useState } from "react";
 export default function RemoteButton() {
   const [count, setCount] = useState(0);
   return (
-    <button onClick={() => setCount(count + 1)}>Remote Button {count}</button>
+    <button onClick={() => setCount(count + 1)}>Remote Button - {count}</button>
   );
 }
