989deccaa9cfd6f12037dbd97ae045d369736111
Danik499 <danikshcherban499@gmail.com>
2026-02-19 20:18:01 +0200

    first commit

---
+++ src/components/RemoteButton.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+export default function RemoteButton() {
+  const [count, setCount] = useState(0);
+  return (
+    <button onClick={() => setCount(count + 1)}>Remote Button {count}</button>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/remote-app-v2/",
+  plugins: [
+    react(),
+    federation({
+      name: "remote",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./RemoteButton": "./src/components/RemoteButton.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+  preview: {
+    port: 5001,
+    strictPort: true,
+    cors: true,
+  },
+});
