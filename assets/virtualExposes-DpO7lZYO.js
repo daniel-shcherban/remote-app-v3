5f9f34ba00e0423ba066ed1f4001569cbd317929
Danik499 <danikshcherban499@gmail.com>
2026-03-05 11:53:35 +0200

    Updates

--- assets/virtualExposes-DpO7lZYO.js
+++ assets/virtualExposes-DpO7lZYO.js
@@ -14,7 +14,7 @@ const exposesMap = {
         }
       ,
         "./Todos": async () => {
-          const importModule = await __vitePreload(() => import('./todos-D2gGJeRT.js').then(n => n.u),true              ?[]:void 0);
+          const importModule = await __vitePreload(() => import('./todos-C0YytxQY.js'),true              ?[]:void 0);
           const exportModule = {};
           Object.assign(exportModule, importModule);
           Object.defineProperty(exportModule, "__esModule", {
